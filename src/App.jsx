4411a1d3ecf5e61e925cd815d76c6ae5e9506822
manish <manishkr9547@gmail.com>
2025-03-28 17:17:11 +0530

    add CompanyLogo component and implement marquee animation; update App layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import "./App.css"
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
+import CompanyLogo from "./components/CompanyLogo";
 
 const App = () => {
   return (
-    <div>
-      <Nav />
-      <Hero />
-    </div>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Nav />
+        <Hero />
+        <CompanyLogo />
+      </div>
+    </main>
   );
 };
 
